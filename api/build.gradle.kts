1b8bf1f615738116d05cbbb059260921d0c44b90
maleitenbauer <Leitenbauer.markus@gmx.at>
2026-09-19 23:41:19 +0200

    Added test fix

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -44,6 +44,7 @@ dependencies {
     testImplementation("org.springframework.modulith:spring-modulith-starter-test")
     testImplementation("org.springframework.security:spring-security-test")
     testImplementation("org.springframework.boot:spring-boot-testcontainers")
+    testImplementation("org.testcontainers:junit-jupiter")
     testImplementation("org.testcontainers:postgresql")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
